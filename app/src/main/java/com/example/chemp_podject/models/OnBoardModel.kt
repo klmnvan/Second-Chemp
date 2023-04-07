dfc9c9340b4e16ec717ba6896144f125853ad872
klmnvan <126891264+klmnvan@users.noreply.github.com>
2023-04-07 10:47:55 +0300

    OnBoard swipe configure
    Свайпы в начальном экране исправлены, теперь нельзя свайпать назад. Также была добавлена иконка приложения и изменено название

---
+++ app/src/main/java/com/example/chemp_podject/models/OnBoardModel.kt
@@ -0,0 +1,13 @@
+package com.example.chemp_podject.models
+
+import android.graphics.drawable.Drawable
+
+data class OnBoardModel(
+    var button: String,
+    var title: String,
+    var description: String,
+    var image: Drawable,
+    var p1: Drawable,
+    var p2: Drawable,
+    var p3: Drawable
+)
--- app/src/main/java/com/example/chemp_podject/on_board1.kt
+++ app/src/main/java/com/example/chemp_podject/on_board1.kt
@@ -7,163 +7,102 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chemp_podject.databinding.ActivityOnBoard1Binding
+import com.example.chemp_podject.models.OnBoardModel
 import java.lang.Math.abs
 import java.util.*
 
 //Нужно наследоваться от GestureDetector.OnGestureListener для обработки свайпов
 class on_board1 : AppCompatActivity(), GestureDetector.OnGestureListener {
     private var binding: ActivityOnBoard1Binding? = null
-
-    //Изначально index задаем 0, т.к. у нас это первый кружочек
-    private var index = 0
-
-    //Создаем объект GestureDetector
+    lateinit var queue: Queue<OnBoardModel>
     lateinit var gestureDetector: GestureDetector
-
-    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoard1Binding.inflate(layoutInflater)
         setContentView(binding!!.root)
-        // Инициализируем переменную gestureDetector
         gestureDetector = GestureDetector(this@on_board1, this@on_board1)
-        binding!!.textTopscreen.setOnClickListener()
-        {
+        queue =
+            LinkedList(listOf(
+                OnBoardModel("Пропустить","Анализы", "Экспресс сбор и получение проб",
+                    getDrawable(R.drawable.icon_analizi)!!, getDrawable(R.drawable.point_style_blue)!!,
+                    getDrawable(R.drawable.point_style_stroke)!!, getDrawable(R.drawable.point_style_stroke)!!),
+                OnBoardModel("Пропустить","Уведомления", "Вы быстро узнаете о результатах",
+                    getDrawable(R.drawable.icon_yved)!!, getDrawable(R.drawable.point_style_stroke)!!,
+                    getDrawable(R.drawable.point_style_blue)!!, getDrawable(R.drawable.point_style_stroke)!!),
+                OnBoardModel("Завершить","Мониторинг", "Наши врачи всегда наблюдают за вашими показателями здоровья",
+                    getDrawable(R.drawable.icon_monitor)!!, getDrawable(R.drawable.point_style_stroke)!!,
+                    getDrawable(R.drawable.point_style_stroke)!!, getDrawable(R.drawable.point_style_blue)!!),
 
-            val textTopscreen = Intent(this@on_board1, Input_and_register::class.java)
-            startActivity(textTopscreen)
-        }
-        //вызываем метод
+            ))
+        enterOnBoard(queue!!.poll())
         init()
     }
 
     /**
-     * init - отдельный метод в который кладем события (нажатие на кнопку, изменение текста и т.п.)
+     * отдельный метод, в который кладем события (в нашем случае нажатие на кнопку)
      */
-    private fun init() {
-        with(binding!!) {
-            pointFirst.setOnClickListener() //Нажатие на первый кружочек
-            {
-                changedText(0)
-            }
-            pointSecond.setOnClickListener() //Нажатие на второй кружочек
-            {
-                changedText(1)
-            }
-            pointThird.setOnClickListener() //Нажатие на третий кружочек
-            {
-                changedText(2)
+    fun init(){
+        with(binding!!){
+            textTopscreen.setOnClickListener(){
+                startActivity(Intent(this@on_board1, Input_and_register::class.java))
+                finish()
             }
         }
     }
 
-    @SuppressLint("UseCompatLoadingForDrawables")
-            /**
-             * @param point - индекс кружочка в отрисовке
-             */
-    fun changedText(point: Int) {
-        with(binding!!) {
-            /*
-            В зависимости от точки меняем текст, цвет и т.п.
-             */
-            when (point) {
 
-                0 -> {
-                    textView2.text = "Анализы"
-                    textView3.text = "Экспресс сбор и получение проб"
-                    textTopscreen.text = "Пропустить"
-                    pointFirst.background = getDrawable(R.drawable.shape_circle_blue)
-                    pointSecond.background = getDrawable(R.drawable.shape_circle)
-                    pointThird.background = getDrawable(R.drawable.shape_circle)
-                    picture.setImageDrawable(getDrawable(R.drawable.icon_analizi))
-                    index = 0
-                }
-                1 -> {
-                    textView2.text = "Уведомления"
-                    textView3.text = "Вы быстро узнаете о результатах"
-                    textTopscreen.text = "Пропустить"
-                    pointFirst.background = getDrawable(R.drawable.shape_circle)
-                    pointSecond.background = getDrawable(R.drawable.shape_circle_blue)
-                    pointThird.background = getDrawable(R.drawable.shape_circle)
-                    picture.setImageDrawable(getDrawable(R.drawable.icon_yved))
-                    index = 1
-                }
-                2 -> {
-                    textView2.text = "Мониторинг"
-                    textView3.text = "Наши врачи всегда наблюдают \nза вашими показателями здоровья"
-                    textTopscreen.text = "Завершить"
-                    pointFirst.background = getDrawable(R.drawable.shape_circle)
-                    pointSecond.background = getDrawable(R.drawable.shape_circle)
-                    pointThird.background = getDrawable(R.drawable.shape_circle_blue)
-                    picture.setImageDrawable(getDrawable(R.drawable.icon_monitor))
-                    index = 2
-                }
-            }
+    /**
+     * enterOnBoard - отдельный метод, с помощью которого мы берем данные из заполненного листа
+     * (LinkedList) и помещаем их в нужные объекты layout
+     */
+    fun enterOnBoard(onBoardModel: OnBoardModel){
+        with(binding!!){
+            textTopscreen.text = onBoardModel.button
+            textName.text = onBoardModel.title
+            textDes.text = onBoardModel.description
+            picture.setImageDrawable(onBoardModel.image)
+            pointFirst.setImageDrawable(onBoardModel.p1)
+            pointSecond.setImageDrawable(onBoardModel.p2)
+            pointThird.setImageDrawable(onBoardModel.p3)
         }
     }
 
-    //Все методы создаются по умолчанию, мы изменяем только onFling
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return if (gestureDetector.onTouchEvent(event!!)) {
+        return if (gestureDetector.onTouchEvent(event!!)){
             true
-        } else {
-            super.onTouchEvent(event)
+        } else{
+            return super.onTouchEvent(event)
         }
     }
-
-    override fun onDown(p0: MotionEvent): Boolean {
+    override fun onDown(e: MotionEvent): Boolean {
         return false
     }
 
-    override fun onShowPress(p0: MotionEvent) {
-
+    override fun onShowPress(e: MotionEvent) {
     }
 
-    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
         return false
     }
 
-    override fun onScroll(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+    override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
         return false
     }
 
-    override fun onLongPress(p0: MotionEvent) {
-        TODO("Not yet implemented")
+    override fun onLongPress(e: MotionEvent) {
     }
 
-    override fun onFling(
-        e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float
-    ): Boolean {
-        try {
-            /*
-            e1 - начальная точка касания (параметр x - ось Ox, параметр y - ось Oy)
-            e2 - конечная точка касания
-            diffY - движение по вертикали
-            diffX = движение по горизонтали
-            */
-            val diffY = e2.y - e1.y
-            val diffX = e2.x - e1.x
-            if (abs(diffX) > abs(diffY)) {
-                if (abs(diffX) > 100 && abs(velocityX) > 100) {
-                    if (diffX > 0) {
-                        /*
-                        Свайп слева на право, уменьшаем индекс, если он выходит за границы, то возвращаем в два (в конец)
-                        */
-                        index--
-                        if (index == -1) index = 2
-                        changedText(index)
-                    } else {
-                        /*
-                      Свайп справо на лево, увеличиваем индекс, если он выходит за границы, то возвращаем в ноль (в начало)
-                      */
-                        index++
-                        if (index == 3) index = 0
-                        changedText(index)
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        val diffY = e2.y - e1.y
+        val diffX = e2.x - e1.x
+        if (abs(diffX)> abs(diffY)){
+            if(abs(diffX) > 100 && abs(velocityX) > 100){
+                if(diffX < 0){
+                    if(queue!!.size!=0){
+                        enterOnBoard(queue.poll())
                     }
                 }
             }
-        } catch (exception: Exception) {
-            exception.printStackTrace()
         }
         return true
     }
